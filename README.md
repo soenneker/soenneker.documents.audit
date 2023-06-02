8af4fb2fa984f7c000f1938582968c897c6c7575
Jake Soenneker <jake@soenneker.com>
2023-06-01 20:08:17 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Documents.Audit.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Documents.Audit/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.documents.audit/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.documents.audit/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Documents.Audit.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Documents.Audit/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Documents.Audit
+### A derivation of Document that specifically exists for audit purposes
+
+## Installation
+
+```
+Install-Package Soenneker.Documents.Audit
+```
---
+++ src/AuditDocument.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+using Ardalis.SmartEnum.SystemTextJson;
+using Newtonsoft.Json;
+using Soenneker.Enums.EventType;
+
+namespace Soenneker.Documents.Audit;
+
+/// <summary>
+/// A derivation of Document that specifically exists for audit purposes <para/>
+/// The PartitionKey of the AuditDocument is the Document Id of the target entity
+/// </summary>
+public class AuditDocument : Document.Document
+{
+    /// <summary>
+    /// Document Id of the target entity
+    /// </summary>
+    [JsonPropertyName("partitionKey")]
+    [JsonProperty("partitionKey")]
+    public override string PartitionKey { get; set; } = default!;
+
+    [JsonPropertyName("entity")]
+    [JsonProperty("entity")]
+    public object? Entity { get; set; }
+
+    [JsonPropertyName("entityId")]
+    [JsonProperty("entityId")]
+    public string EntityId { get; set; } = default!;
+
+    [JsonPropertyName("entityType")]
+    [JsonProperty("entityType")]
+    public string EntityType { get; set; } = default!;
+
+    [JsonPropertyName("eventType")]
+    [JsonProperty("eventType")]
+    [System.Text.Json.Serialization.JsonConverter(typeof(SmartEnumNameConverter<EventType, int>))]
+    [Newtonsoft.Json.JsonConverter(typeof(Ardalis.SmartEnum.JsonNet.SmartEnumNameConverter<EventType, int>))]
+    public EventType EventType { get; set; } = default!;
+
+    [JsonPropertyName("userId")]
+    [JsonProperty("userId")]
+    public string? UserId { get; set; }
+}
